113c5a5e7ba8b9d84baa372d5ea3271f07578fc6
Ruslan Bekshenev <ruslanbekshenev@yandex.ru>
2019-07-07 19:32:48 +0500

    done map

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,20 +1,26 @@
 $(function() {
-  let flag = true;
+  //events bar
+  let flag = false;
   $(".eventsbar__arrow button").click(function() {
-    if (flag) {
-      $(".eventsbar__content")
+    if (!flag) {
+      $(".eventsbar")
         .addClass("is_open")
         .removeClass("is_close");
       $(this).css({ transform: "rotate(0deg)" });
       $("body").css({ "overflow-y": "scroll" });
     } else {
-      $(".eventsbar__content")
+      $(".eventsbar")
         .addClass("is_close")
         .removeClass("is_open");
-      console.log("close");
       $(this).css({ transform: "rotate(180deg)" });
       $("body").css({ "overflow-y": "hidden" });
     }
     flag = !flag;
+  });
+
+  //map nav
+  $(".map__menu .btn-map").click(function() {
+    $(".map__menu .btn-map").removeClass("active");
+    $(this).addClass("active");
   });
 });
